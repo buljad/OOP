46d267cc9a70d91e07d1a17891c1472687d37b6d
buljad <i.zhuravskii@g.nsu.ru>
2022-10-02 23:43:00 +0700

    deleted Task_1_1_1 from this branch

--- Task_1_1_1/script.bat
+++
@@ -1,6 +0,0 @@
-javadoc -d .\make\ -sourcepath .\src\main\java -subpackages ru.nsu.izhuravskii
-
-javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\izhuravskii\heapsort.java
-
-mkdir .\make\jar
-jar cf .\make\jar\heapsort.jar -C .\make\bin .
