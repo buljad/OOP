9d21d9ea719a7ee11f173ce5736865273db2698d
buljad <i.zhuravskii@g.nsu.ru>
2022-10-11 09:45:42 +0700

    Remade generic types form Number to <T>, created packages, added script.bat

---
+++ Task_1_2_1/src/main/java/ru/nsu/izhuravskii/Stack.java
@@ -0,0 +1,48 @@
+package ru.nsu.izhuravskii;
+
+import java.util.Arrays;
+
+import java.lang.Object;
+
+public class Stack <T>{
+    public int cnt;
+    public T[] stack;
+    public int stackLen;
+
+
+    public void stack_init(int capacity) {
+        cnt = 0;
+        stackLen = capacity;
+        stack = (T[]) new Object[stackLen];
+    }
+    public void push(T obj) {
+        cnt++;
+        if (cnt > stackLen) {
+            stackLen *= 2;
+            stack = Arrays.copyOf(stack, stackLen);
+        }
+        stack[cnt--] = obj;
+    }
+
+    public void push_stack(T[] new_stack) {
+        for(T i : new_stack) {
+            push(i);
+        }
+    }
+
+    public void pop() {
+        if (cnt != 0) {
+            stack[cnt] = null;
+            cnt--;
+        }
+    }
+    public void pop_stack(int num) {
+        for(int i = 0; i < num; i++) {
+            pop();
+        }
+    }
+
+    public int count() {
+        return cnt;
+    }
+}
