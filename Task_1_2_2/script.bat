c1fa0d3b65bf6cc2e88a45fb1d0f74bc2f3c973a
buljad <i.zhuravskii@g.nsu.ru>
2022-10-16 23:00:11 +0700

    Modified .gitignore

--- Task_1_2_2/script.bat
+++
@@ -1,6 +0,0 @@
-javadoc -d .\make\ -sourcepath .\src\main\java -subpackages ru.nsu.izhuravskii
-
-javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\izhuravskii\Tree.java
-
-mkdir .\make\jar
-jar cf .\make\jar\Tree.jar -C .\make\bin .
