cd4f0e7ce092ef130a4bf9f099fa8933c88ae3e7
buljad <i.zhuravskii@g.nsu.ru>
2022-10-19 02:16:54 +0700

    Added new functions/methods to Tree.java

---
+++ Task_1_2_2/src/main/java/ru/nsu/izhuravskii/Tree.java
@@ -0,0 +1,72 @@
+package ru.nsu.izhuravskii;
+
+import org.jetbrains.annotations.NotNull;
+import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Tree<T> {
+
+    private T value;
+    private Tree<T> parent;
+    private List<Tree<T>> children;
+
+    public void treeInit (T val) {
+        this.value = val;
+        this.parent = null;
+        this.children = new ArrayList<>();
+    }
+
+    public void addChild (@NotNull Tree<T> child, T val) {
+        child.parent = this;
+        child.value = val;
+        children.add(child);
+    }
+
+    public void removeChild (@NotNull Tree<T> child) {
+        if (child.children != null) {
+            child.parent.children.addAll(child.children);
+            for (Tree <T> t : child.children) {
+                child.children.remove(t);
+            }
+        }
+        child.parent.children.remove(child);
+    }
+
+    public void removeChildren(@NotNull Tree<T> member) {
+        for (Tree<T> t : member.children) {
+            member.children.remove(t);
+        }
+    }
+
+    public void showChildren(@NotNull Tree <T> member) {
+        for (Tree <T> t : member.children) {
+            System.out.print(t);
+        }
+    }
+
+    public Tree <T> getParent(Tree <T> p) {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Tree<?> obj = (Tree<?>) o;
+        return Objects.equals(value, obj.value)
+                && Objects.equals(children, obj.children);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, children);
+    }
+}
+
+
---
+++ Task_1_2_2/src/test/java/ru/nsu/izhuravskii/TreeTests.java
@@ -0,0 +1,4 @@
+package ru.nsu.izhuravskii;
+
+public class TreeTests {
+}
