248441d85d0d4b8eac1e4a47756a1456dd7d99e1
buljad <i.zhuravskii@g.nsu.ru>
2022-12-07 23:27:58 +0700

    Soft deadline commit, only string split added

---
+++ Task_1_5_1/src/main/java/ru/nsu/izhuravskii/Calculator.java
@@ -0,0 +1,10 @@
+package ru.nsu.izhuravskii;
+
+public class Calculator {
+
+    //будет double, а не void
+    public static void calculate(String expression) {
+        String[] symbols = expression.split(" ");
+
+    }
+}
