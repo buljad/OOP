b57699b1a7177deb2676a615a541a1361fdd6bd4
buljad <i.zhuravskii@g.nsu.ru>
2022-12-21 21:19:10 +0700

    Add modificationCounter incrementations

--- Task_1_2_2/src/main/java/ru/nsu/izhuravskii/Tree.java
+++ Task_1_2_2/src/main/java/ru/nsu/izhuravskii/Tree.java
@@ -17,6 +17,7 @@ public class Tree<T> implements Iterable <T> {
         this.value = val;
         this.parent = null;
         this.children = new ArrayList<>();
+        this.modificationCounter = 0;
     }
     public void setParent(Tree<T> parent) { this.parent = parent; };
     public Tree <T> getParent() { return this.parent; }
@@ -25,6 +26,16 @@ public class Tree<T> implements Iterable <T> {
     public List<Tree <T>> getChildren () { return this.children; }
     public void setSearch(boolean type) { this.search = type; }
     public boolean getSearch() { return this.search; }
+    public int getModificationCounter() {
+        return modificationCounter;
+    }
+    public Tree<T> getRoot() {
+        Tree<T> step = this;
+        while (step.getParent() != null) {
+            step = step.getParent();
+        }
+        return step;
+    }
 
     public Tree<T> addChild (T val) {
         Tree <T> child = new Tree<>();
@@ -32,19 +43,17 @@ public class Tree<T> implements Iterable <T> {
         child.setParent(this);
         child.children = new ArrayList<>();
         this.children.add(child);
+        this.modificationCounter ++;
+        this.getRoot().modificationCounter ++;
         return child;
     }
-
-    public int getModificationCounter() {
-        return modificationCounter;
-    }
-
     public Tree<T> addChild (Tree<T> parentVertex, T val) {
         Tree <T> child = new Tree<>();
-        child.value = val;
-        child.parent = parentVertex;
+        child.setValue(val);
+        child.setParent(parentVertex);
         child.children = new ArrayList<>();
         parentVertex.children.add(child);
+        parentVertex.getRoot().modificationCounter ++;
         return child;
     }
 
@@ -54,6 +63,7 @@ public class Tree<T> implements Iterable <T> {
             this.addChild(i.value);
         }
         children.remove(index);
+        this.getRoot().modificationCounter ++;
     }
 
     @Override
