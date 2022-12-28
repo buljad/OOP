edfe0330a8aefdba982251a5614eb971f65959cc
buljad <buljad@users.noreply.github.com>
2022-12-28 09:15:14 +0000

    Deploying to gh-pages from @ buljad/OOP@a59c10cac20de8fed272ef37a221f0502a5faecf 🚀

---
+++ Task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.izhuravskii","l":"BreadthFirstSearchIterator"},{"p":"ru.nsu.izhuravskii","l":"DepthFirstSearchIterator"},{"p":"ru.nsu.izhuravskii","l":"Tree"}];updateSearchResults();
