5b2522ad33dc4cc84250b1ebc680a442cb6b331a
buljad <i.zhuravskii@g.nsu.ru>
2023-03-07 01:50:58 +0700

    Make ParalleStreamFinder static and add tests for ParallelStreamMode

--- Task_2_1_1/src/main/java/ru/nsu/izhuravskii/ParallelStreamMode.java
+++ Task_2_1_1/src/main/java/ru/nsu/izhuravskii/ParallelStreamMode.java
@@ -5,7 +5,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class ParallelStreamMode {
-    public boolean parallelStreamFinder (@NotNull List<Long> numbers){
+    public static boolean parallelStreamFinder (@NotNull List<Long> numbers){
         return numbers.parallelStream().anyMatch(NotPrimeNumbers::isNotPrime);
     }
 }
---
+++ Task_2_1_1/src/test/java/ru/nsu/izhuravskii/ParallelStreamModeTests.java
@@ -0,0 +1,50 @@
+package ru.nsu.izhuravskii;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ParallelStreamModeTests {
+    @Test
+    public void notPrimeTest() {
+        List<Long> numbers = Arrays.asList(2L, 3L, 5L, 7L, 9L);
+        boolean expectedValue = true;
+        boolean actualValue = ParallelStreamMode.parallelStreamFinder(numbers);
+        Assertions.assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void onlyPrimeTest() {
+        List<Long> numbers = Arrays.asList(2L, 3L, 5L, 7L, 11L);
+        boolean expectedValue = false;
+        boolean actualValue = ParallelStreamMode.parallelStreamFinder(numbers);
+        Assertions.assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void onlyNotPrimeTest() {
+        List<Long> numbers = Arrays.asList(4L, 6L, 8L, 10L, 12L);
+        boolean expectedValue = true;
+        boolean actualValue = ParallelStreamMode.parallelStreamFinder(numbers);
+        Assertions.assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void onlyOneNumberTest() {
+        List<Long> numbers = Arrays.asList(1L, 1L, 1L, 1L, 1L);
+        boolean expectedValue = false;
+        boolean actualValue = ParallelStreamMode.parallelStreamFinder(numbers);
+        Assertions.assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void mixNotPrimeTest() {
+        List<Long> numbers = Arrays.asList(19L, 29L, 1L, 4L, 5L);
+        boolean expectedValue = true;
+        boolean actualValue = ParallelStreamMode.parallelStreamFinder(numbers);
+        Assertions.assertEquals(expectedValue, actualValue);
+
+    }
+}
