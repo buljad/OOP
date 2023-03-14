a32ba744fa37f2c8835c381ac9e21db5b5701e05
buljad <i.zhuravskii@g.nsu.ru>
2023-03-14 23:41:00 +0700

    Add Cook and Deliver modules

--- Task_2_2_1/src/main/java/ru/nsu/izhuravskii/Cook.java
+++ Task_2_2_1/src/main/java/ru/nsu/izhuravskii/Cook.java
@@ -1,6 +1,7 @@
 package ru.nsu.izhuravskii;
 
 public class Cook {
+    String currentOrder;
     public Cook(String name, int experience) {
 
     }
--- Task_2_2_1/src/main/java/ru/nsu/izhuravskii/Deliver.java
+++ Task_2_2_1/src/main/java/ru/nsu/izhuravskii/Deliver.java
@@ -1,6 +1,8 @@
 package ru.nsu.izhuravskii;
 
+import java.util.Queue;
 public class Deliver {
+    Queue<String> currentOrders;
     public Deliver(String name, int deliveryExperience, int capacity) {
 
     }
