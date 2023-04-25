3ae6da1f42b43297f50a77a6b179a54dc0624b78
buljad <i.zhuravskii@g.nsu.ru>
2023-04-25 14:44:27 +0700

    Add findPrefix and search of Substring algorithms

--- Task_1_3_1/src/main/java/ru/nsu/izhuravskii/SubstringSearch.java
+++ Task_1_3_1/src/main/java/ru/nsu/izhuravskii/SubstringSearch.java
@@ -3,6 +3,9 @@ package ru.nsu.izhuravskii;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 /*public class SubstringSearch {
     public void InputHandler() throws IOException {
@@ -17,20 +20,59 @@ import java.io.IOException;
 */
 
 public class SubstringSearch {
+    Reader input;
+    String substring;
 
-    static void search(String pat, String txt) {
-        int l1 = pat.length();
-        int l2 = txt.length();
-        int i = 0, j = l2 - 1;
 
-        for (i = 0, j = l2 - 1; j < l1; ) {
-
-            if (txt.equals(pat.substring(i, j + 1))) {
-                System.out.println("Pattern found at index " + i);
+    public static int[] findPrefixArray(String substring) {
+        int substringLength = substring.length();
+        int[] prefixArray = new int[substringLength + 1];
+        prefixArray[0] = -1;
+        prefixArray[1] = 0;
+        int prefixLength = 0;
+        int currentSymbolId = 1;
+        while (currentSymbolId < substringLength) {
+            if (substring.charAt(prefixLength) == substring.charAt(currentSymbolId)) {
+                prefixLength++;
+                currentSymbolId++;
+                prefixArray[currentSymbolId] = prefixLength;
+            } else if (prefixLength > 0) {
+                prefixLength = prefixArray[prefixLength];
+            } else {
+                currentSymbolId++;
+                prefixArray[currentSymbolId] = 0;
             }
-            i++;
-            j++;
         }
+        return prefixArray;
+    }
+    public static List<Integer> SubstringSearch(Reader file, String substring)
+            throws IOException {
+        int currentId = 0;
+        int idInSubstring = 0;
+        int substringLen = substring.length();
+
+        List<Integer> matchesId = new ArrayList<>();
+        int[] prefixLen = findPrefixArray(substring);
+        int c;
+        while ((c = file.read()) != -1) {
+            char currentLetter = (char) c;
+            if (substring.charAt(idInSubstring
+            ) == currentLetter) {
+                idInSubstring++;
+                currentId++;
+                if (idInSubstring == substringLen) {
+                    matchesId.add(currentId - idInSubstring);
+                    idInSubstring = prefixLen[idInSubstring];
+                }
+            } else {
+                idInSubstring = prefixLen[idInSubstring];
+                if (idInSubstring < 0) {
+                    currentId++;
+                    idInSubstring++;
+                }
+            }
+        }
+        return matchesId;
     }
 }
 
