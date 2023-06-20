07e7abfa09c450fd57222db8f80c43fd7b26aa50
buljad <i.zhuravskii@g.nsu.ru>
2023-06-20 18:31:36 +0700

    Add hard shutdowns, change BlockingQueue syncronized Deque, add timer

--- Task_2_2_1/src/main/java/ru/nsu/izhuravskii/Main.java
+++ Task_2_2_1/src/main/java/ru/nsu/izhuravskii/Main.java
@@ -1,32 +1,37 @@
 package ru.nsu.izhuravskii;
 
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A launch of my pizzeria.
  */
-public class Main implements Runnable{
+public class Main{
     public static void main(String[] args){
-        Main work = new Main();
-        new Thread(work).start();
-    }
-    Scanner scanner = new Scanner(System.in);
-    @Override
-    public void run() {
+        Scanner scanner = new Scanner(System.in);
         Pizzeria pizzeria = new Pizzeria("pizzeriaData.json");
         pizzeria.openPizzeria();
         System.out.println("Pizzeria is opened");
-        while(true) {
+        Timer timer = new Timer();
+        final boolean[] cancelFlag = {false};
+        timer.schedule(new java.util.TimerTask() {
+            @Override
+            public void run() {
+                pizzeria.closePizzeria();
+                timer.cancel();
+                System.out.println("Pizzeria is closed");
+                cancelFlag[0] = true;
+            }
+        }, 10000);
+        while(!cancelFlag[0]) {
             if (scanner.hasNextInt()) {
                 int size = scanner.nextInt();
                 if (size == -1) {
-                    try {
-                        pizzeria.closePizzeria();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+                    pizzeria.closePizzeria();
                     System.out.println("Pizzeria is closed");
-                    return;
+                    timer.cancel();
+                    break;
                 }
                 for (int i = 0; i < size; i++) {
                     try {
--- Task_2_2_1/src/main/java/ru/nsu/izhuravskii/Pizzeria.java
+++ Task_2_2_1/src/main/java/ru/nsu/izhuravskii/Pizzeria.java
@@ -3,29 +3,25 @@ package ru.nsu.izhuravskii;
 import static java.lang.Math.ceil;
 import static java.lang.Math.min;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.BlockingQueue;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class Pizzeria {
-    private static BlockingQueue<Order> orderQueue = new LinkedBlockingQueue<>();
-    private static BlockingQueue<Order> stockQueue;
-    private List<Cook> cooks;
-    private List<Deliver> delivers;
-    static int totalBagCapacity = 0;
+    private final static Deque<Order> orderQueue = new ArrayDeque<>();
+    private static Deque<Order> stockQueue;
+    private final List<Cook> cooks;
+    private final List<Deliver> delivers;
+    static int totalCapacity = 0;
     private final ExecutorService cooksPool;
     private final ExecutorService deliversPool;
 
     public Pizzeria(String file) {
         JsonData jsonData = new JsonData(file);
-        stockQueue = new LinkedBlockingQueue<>(jsonData.getStockCapacity());
+        stockQueue = new ArrayDeque<>(jsonData.getStockCapacity());
         this.cooks = jsonData.getCooks();
         this.delivers = jsonData.getDelivers();
-        totalBagCapacity = delivers.stream().mapToInt(Deliver::getCapacity).sum();
+        totalCapacity = delivers.stream().mapToInt(Deliver::getCapacity).sum();
         cooksPool = Executors.newFixedThreadPool(cooks.size());
         deliversPool = Executors.newFixedThreadPool(delivers.size());
     }
@@ -36,44 +32,48 @@ public class Pizzeria {
     }
 
     public static Order takeOrder() throws InterruptedException {
-        return orderQueue.take();
+        synchronized (orderQueue) {
+            while (orderQueue.isEmpty()) {
+                orderQueue.wait();
+            }
+            return orderQueue.pollFirst();
+        }
     }
 
     public void addOrder(Order order) throws InterruptedException {
-        orderQueue.put(order);
-        order.setStatus(Order.Status.ORDERED);
+        synchronized(orderQueue) {
+            orderQueue.add(order);
+            order.setStatus(Order.Status.ORDERED);
+            orderQueue.notifyAll();
+        }
     }
 
 
     public static void stockOrder(Order order) throws InterruptedException {
-        stockQueue.put(order);
-        order.setStatus(Order.Status.IN_STOCK);
-        System.out.println(order);
+        synchronized (stockQueue) {
+                stockQueue.add(order);
+                stockQueue.notifyAll();
+                order.setStatus(Order.Status.IN_STOCK);
+                System.out.println(order);
+        }
     }
 
     public static List<Order> takeFromStock(int capacity) throws InterruptedException {
-        List<Order> orders = new ArrayList<>();
-        int count = (int) min(ceil(
-                ((double) stockQueue.size() / totalBagCapacity) * capacity + 1), capacity);
-        for (int i = 0; i < count; i++) {
-            orders.add(stockQueue.take());
+        synchronized (stockQueue) {
+            while(stockQueue.isEmpty()) {
+                stockQueue.wait();
+            }
+            List<Order> orders = new ArrayList<>();
+            int count = (int) min(ceil(
+                    ((double) stockQueue.size() / totalCapacity) * capacity + 1), capacity);
+            for (int i = 0; i < count; i++) {
+                orders.add(stockQueue.pollFirst());
+            }
+            return orders;
         }
-        return orders;
     }
-    public void closePizzeria() throws InterruptedException {
-        while (true) {
-            if (orderQueue.isEmpty() && cooks.stream().noneMatch(Cook::isCooking)) {
-                cooksPool.shutdown();
-                System.out.println("Cooks are going home");
-                break;
-            }
-        }
-        while (true) {
-            if (stockQueue.isEmpty() && delivers.stream().noneMatch(Deliver::isOnTheWay)) {
-                deliversPool.shutdown();
-                System.out.println("Delivers are going home");
-                break;
-            }
-        }
+    public void closePizzeria(){
+            cooksPool.shutdownNow();
+            deliversPool.shutdownNow();
     }
 }
