838d78ff71dc429f9337873c5d96984efcb70372
buljad <i.zhuravskii@g.nsu.ru>
2023-06-21 15:55:22 +0700

    Add javadoc comment

--- Task_2_2_1/src/test/java/ru/nsu/izhuravskii/PizzeriaTests.java
+++ Task_2_2_1/src/test/java/ru/nsu/izhuravskii/PizzeriaTests.java
@@ -2,6 +2,11 @@ package ru.nsu.izhuravskii;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Test for launching pizzeria and
+ * successful build of project on GitHub.
+ */
+
 public class PizzeriaTests {
     @Test
     public void test() {
